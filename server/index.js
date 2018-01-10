3fa634e41e6996f7ebc95732982d560570ddaad3
diego-lipinski-de-castro <diegocastroh20@gmail.com>
2018-01-10 20:37:05 -0200

    reworking server

--- server/index.js
+++ server/index.js
@@ -1,64 +1,30 @@
 const admin = require('firebase-admin')
+const functions = require('firebase-functions')
 const axios = require('axios')
 const schedule = require('node-schedule')
 
 const serviceAccount = require('./key.json')
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://crypto-tracker-dc.firebaseio.com'
+  credential: admin.credential.cert(serviceAccount)
 })
 
-const db = admin.database()
+// admin.initializeApp({
+//   credential: admin.credential.applicationDefault()
+// })
 
-const percentage_ref = db.ref('percentage')
+// admin.initializeApp(functions.config().firebase)
 
-const fetchData = async (api_url) => {
+const db = admin.firestore()
 
-  try {
+const coinsRef = db.collection('coins')
 
-    const res = await axios.get(api_url)
-    const data = res.data    
-    return data
-
-  } catch (error) {
-    return error
-  }
-
-}
-
-const getData = async () => {
-
-  try {
-
-    let data = await Promise.all([
-      fetchData('https://api.coinmarketcap.com/v1/ticker/?limit=9999')
-    ]).then(res => {
-      saveData(res)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-
-    return data
-
-  } catch(error) {
-    return error
-  }
-
-}
-
-const saveData = (data) => {
-  percentage_ref.set(data[0])
-}
-
-schedule.scheduleJob('*/5 * * * *', () => {
-  getData()
-})
+// schedule.scheduleJob('*/5 * * * *', () => {
+//   getData()
+// })
 
 /*
-  - percentage - https://coinmarketcap.com/api/
-  - raw data - https://www.cryptocompare.com/api/
-  - history - https://www.cryptocompare.com/api/
-  - marketshare - https://www.cryptonator.com/api
+  - https://coinmarketcap.com/api/
+  - https://www.cryptocompare.com/api/
+  - https://www.cryptonator.com/api
 */
