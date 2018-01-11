f567946d24ef75f2184e0c08fbdd5297a42282ac
diego-lipinski-de-castro <diegocastroh20@gmail.com>
2018-01-11 18:44:04 -0200

    server changes

--- server/index.js
+++ server/index.js
@@ -19,12 +19,43 @@ const db = admin.database()
 
 const coins = require('./coins.json')
 
+const refs = coins.map(coin => {
+  return {
+    [coin]: db.ref(coin)
+  }
+})
+
+const coinmarketcap_end_point = 'https://api.coinmarketcap.com/v1/ticker/?limit=9999'
+
+getData = async (api_url) => {
+
+  try {
+
+    return await axios.get(api_url)
+
+  } catch(error) {
+    console.log(error)
+  }
+
+}
+
+getData(coinmarketcap_end_point)
+.then(res => {
+  console.log(res)
+
+})
+.catch(error => {
+  console.log(error)
+})
+
+// const end_points_cryptonator = coins.map(coin => {
+//   return `https://api.cryptonator.com/api/full/${coin}-usd`
+// })
+
+// const end_points = [
+//   'https://www.cryptocompare.com/api/'
+// ]
+
 // schedule.scheduleJob('*/5 * * * *', () => {
 //   getData()
 // })
-
-/*
-  - https://coinmarketcap.com/api/
-  - https://www.cryptocompare.com/api/
-  - https://www.cryptonator.com/api
-*/
